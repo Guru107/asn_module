9eef9cc11a6d1f3dbef6e447cf08795394a819eb
Guru107 <guruak107@gmail.com>
2026-04-05 23:32:54 +0530

    test(cypress): add nightly Scan Station E2E spec with API seeding

---
+++ cypress/integration/nightly/scan_station_nightly.js
@@ -0,0 +1,28 @@
+const route = (path) => {
+	const p = path.replace(/^\//, "");
+	return `/${Cypress.env("routePrefix")}/${p}`;
+};
+
+context("Scan Station nightly", () => {
+	let seededData;
+
+	before(() => {
+		cy.login();
+		cy.call("asn_module.utils.cypress_helpers.seed_scan_station_context").then((result) => {
+			seededData = result.message || result;
+		});
+	});
+
+	it("renders scan input", () => {
+		cy.visit(route("scan-station"));
+		cy.get(".scan-input", { timeout: 20000 }).should("be.visible");
+	});
+
+	it("accepts scan code and shows success or expected feedback", () => {
+		cy.visit(route("scan-station"));
+		cy.get(".scan-input", { timeout: 20000 }).should("be.visible");
+		cy.get(".scan-input").clear();
+		cy.get(".scan-input").type(seededData.scan_code + "{enter}");
+		cy.get(".scan-result, .scan-success, .scan-error", { timeout: 20000 }).should("be.visible");
+	});
+});
