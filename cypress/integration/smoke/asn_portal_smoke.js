3b97ee3bccdb1f4332712fadebcedc5cc8138d05
Guru107 <guruak107@gmail.com>
2026-04-07 15:32:31 +0530

    test(e2e): add 4 new smoke spec files

---
+++ cypress/integration/smoke/asn_portal_smoke.js
@@ -0,0 +1,26 @@
+const route = (path) => {
+    const p = path.replace(/^\//, "");
+    return `/${Cypress.env("routePrefix")}/${p}`;
+};
+
+context("ASN portal smoke", () => {
+    let seededData;
+
+    before(() => {
+        cy.login();
+        cy.call("asn_module.utils.cypress_helpers.seed_supplier_context").then((result) => {
+            seededData = result.message || result;
+        });
+    });
+
+    it("portal user can see their ASN list", () => {
+        cy.visit(route("asn"), { failOnStatusCode: false });
+        cy.get(".page-head, .standard-list-section, .list-row-head", { timeout: 20000 }).should("exist");
+    });
+
+    it("opens ASN detail from portal list", () => {
+        cy.visit(route("asn"), { failOnStatusCode: false });
+        cy.get(".list-row", { timeout: 15000 }).first().click();
+        cy.get(".page-head", { timeout: 20000 }).should("exist");
+    });
+});
