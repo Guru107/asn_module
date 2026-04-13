adcdc8120dec5c827d10e1ec254316394df7be75
Guru107 <guruak107@gmail.com>
2026-04-13 17:39:14 +0530

    test(property): make smoke test discoverable

--- asn_module/property_tests/test_asn_new_services_properties.py
+++ asn_module/property_tests/test_asn_new_services_properties.py
@@ -1,17 +1,19 @@
 from hypothesis import given
 from hypothesis import settings as hypothesis_settings
+from frappe.tests import UnitTestCase
 
 from asn_module.property_tests import settings as property_settings
 from asn_module.property_tests.strategies import scan_text
 
 
 def _identity(x):
-    return x
+	return x
 
 
-@given(scan_text)
-def test_property_harness_smoke_identity(text_value):
-    assert _identity(text_value) == text_value
-    expected_max_examples = 80 if property_settings.PROFILE == "ci" else 300
-    assert hypothesis_settings.default.max_examples == expected_max_examples
-    assert hypothesis_settings.default.deadline is None
+class TestPropertyHarness(UnitTestCase):
+	@given(scan_text)
+	def test_property_harness_smoke_identity(self, text_value):
+		self.assertEqual(_identity(text_value), text_value)
+		expected_max_examples = 80 if property_settings.PROFILE == "ci" else 300
+		self.assertEqual(hypothesis_settings.default.max_examples, expected_max_examples)
+		self.assertIsNone(hypothesis_settings.default.deadline)
